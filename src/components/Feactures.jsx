2a68d0c13152de7c2a80d7600642d71a3c093039
Harsh <harshhujare5147@gmail.com>
2025-06-14 09:29:09 +0530

    tilt effect

--- src/components/Feactures.jsx
+++ src/components/Feactures.jsx
@@ -1,4 +1,39 @@
-import react from 'react'
+import react, { useRef, useState } from 'react'
+
+
+const BentoTilt =({children,className=''})=>{
+const [TransformStyle,setTransformStyle]=useState('')
+const itemRef =useRef();
+const HandelMouseMove =(e)=>{
+ if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (event.clientX - left) / width;
+    const relativeY = (event.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 5;
+    const tiltY = (relativeX - 0.5) * -5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
+    setTransformStyle(newTransform);
+};
+const HandelMouseLeav=()=>{
+     setTransformStyle("");
+};
+return(
+<div className={className}  ref={itemRef} onMouseMove={HandelMouseMove} onMouseLeave={HandelMouseLeav} style={{transform:TransformStyle}}>
+
+
+{children}
+
+</div>
+
+
+)
+
+}
 
 
 
@@ -29,7 +64,7 @@ import react from 'react'
 const Feactures =()=>{
 
 return (
-<section className="bg-black pb-52" >
+<section className="bg-black pb-10" >
 <div className="container mx-auto px-3 md:px-10"> 
 <div className=" px-5 py-32 " >
 
@@ -45,21 +80,46 @@ welcome to the world of AI,
 
 </div>
 
-<div className="border-hsla relative mb-7 h-96 rounded-md md:h-[65vh] w-full overflow-hidden">
+<BentoTilt className="border-hsla relative mb-7 h-96 rounded-md md:h-[65vh] w-full overflow-hidden">
 <BentoCard  
 src="videos/feature-1.mp4"
 tital={<>radia<b>n</b>t</>}
 descrption="A platform for AI enthusiasts to explore and innovate. which is a platform for AI enthusiasts to explore and innovate. It provides a space for developers and enthusiasts to come together, share ideas, and push the boundaries of artificial intelligence. "
 />
-<div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
+</BentoTilt>
+<div className="grid h-[100vh] w-full grid-cols-2 grid-rows-3 gap-7">
 
 
+<BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
 
+<BentoCard  
+src="videos/feature-2.mp4" 
+tital={<> <b>ZigmA</b></>}
+descrption="An anime and gaming-inspired NFT collection - the IP primed for expansion."
 
+/>
 
+</BentoTilt>
 
+<BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+<BentoCard  
+src="videos/feature-3.mp4" 
+tital={<> <b>Azul</b></>}
+descrption="An anime and gaming-inspired NFT collection - the IP primed for expansion."
 
-</div>
+/>
+
+</BentoTilt>
+<BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+<BentoCard  
+src="videos/feature-4.mp4" 
+tital={<> <b>Nexus</b></>}
+descrption=" cross-world AI Agent - elevating your gameplay to be more fun and productive."
+
+/>
+
+</BentoTilt>
+ 
 
 
 
